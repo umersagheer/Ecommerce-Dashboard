c61b113600e0e02b3957a7d89790a8306022f03f
Muhammad Umer <mhadisb1212@gmail.com>
2024-01-05 03:07:55 +0500

    Added Form, api
    Form,label,input from shadcn, validate with RHF alongWith zod. Then Created api route and the connect it with frontend app.

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,11 +1,53 @@
 "use client";
 
-import React from "react";
+import axios from "axios";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+
 import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/use-store-modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 const StoreModal = () => {
   const storeModal = useStoreModal();
+  const [Loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+
+      const response = await axios.post("/api/stores", values);
+
+      toast.success("Store created successfully");
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Modal
@@ -14,7 +56,43 @@ const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Future Create Store
+      <div className="space-y-4 py-2 pb-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={Loading}
+                        placeholder="E-commerce"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+              <Button
+                disabled={Loading}
+                variant={"outline"}
+                onClick={storeModal.onClose}
+              >
+                Cancel
+              </Button>
+              <Button disabled={Loading} type="submit">
+                Create
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </Modal>
   );
 };
